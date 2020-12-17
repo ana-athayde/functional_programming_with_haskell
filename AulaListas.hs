1dc7019878972f399ccf9171c6e5306e5e3f2928
Ana Paula <43683162+ana-athayde@users.noreply.github.com>
2020-12-17 17:42:39 -0300

    Update AulaListas.hs

--- AulaListas.hs
+++ AulaListas.hs
@@ -71,4 +71,124 @@ nultimos n (x:xs) | lenght (x:xs) <= (x:xs)
 --				  | otherwise = nultimos n xs
 
 -- Exercicio 3: Declare uma função que retorne o inverso de uma lista
--- Exemplo? inverso[1,2,3,4] =>[4,3,2,1]
+-- Exemplo: inverso[1,2,3,4] =>[4,3,2,1]
+
+inverso :: [a] - [a]
+inverso [] = []
+inverso lista = last ls : inverso (init ls) 
+
+-- inverso 1:2:3:4:[]
+-- 	4:inverso(1:2:3:[])
+-- 	3:inverso(1:2:[])
+-- 	2:inverso(1:[])
+-- 	1:inverso([])
+-- 	[]
+	
+-- >> 4:3:2:1:[] == []
+
+-- ++
+-- 1:2:3:[] == [1,2,3] : [4,5,6] Não funciona porque esse operador só permite um numero antes dos dois pontos
+-- por isso utilizamos o ++, porque permite uma lista antes e uma depois
+-- : elemento > lista
+-- ++ lista > lista
+
+inverso' [] = []
+inverso' (x:xs) = inverso xs ++ [x]
+
+-- inverso' [1,2,3,4]
+-- (inverso' [2,3,4] ++ [1])
+-- (inverso' [3,4] ++ [2])
+-- (inverso' [4] ++ [3])
+-- (inverso' [] ++ [4])
+-- []
+
+{- [4] ++ [3] ++ [2] ++ [1] == [4,3,2,1] -}
+
+-- Exercício 4: Reescreva a função nultimos utilizando a função inverso.
+
+nultimos' n xs = inverso (take n (inverso xs))
+
+{-2 [1,2,3,4]
+	inverso xs>> [4,3,2,1]
+	take 2 xs >> [4,3]
+	inverso >> [3,4]
+	
+take-}
+
+-- Exercicio 5: Exemplo: repetir 4 10 => [10,10,10,10]
+-- replicate
+
+repetir :: Int -> a -> [a]
+repetir 0 _ = []
+repetir n m = m : repetir (n-1) m
+
+{-
+repetir 4 10
+	10 : repetir (3) 10
+	10 : repetir (2) 10
+	10 : repetir (1) 10
+	10 : repetir (0) 10
+	[]
+	
+>> 10:10:10:10:[]
+-} 
+
+-- Exercicio 6: Dobro de todos os números entre 1 e 100. Compreensão de listas.
+
+f1 [] = []
+f1 (x:xs) = x*2 : f1 xs
+-- f1 [1..100]
+
+f2 = [x*2 | x <- [1..100]] 
+-- f2 [1..100]
+
+-- Dobro dos elementos de 1 á 100, desde que sejam pares
+
+f3 = [x*2 | x <- [1..100], rem x 2 == 0]
+f3' = [x*2 | x <- [1..100], even 2]
+
+--even 10 >> True
+--even 11 >> False
+
+--odd 10 >> False
+--odd 11 >> True
+
+-- Dobro dos pares entre 1 e 100 maiores que 50
+
+f3'' = [x*2 | x <- [1..100], even x, x > 50]
+
+-- Exercicio 7: Implementar a função tamanha (lenght) utilizando compreensão de listas
+
+tamanho xs = sum [1 | _ <- xs]
+
+{-
+[1,2,3,4,5]
+[1,1,1,1,1]
+sum
+-}
+
+{-
+[1] :: [Int]
+[[1],[2]] :: [[Int]]
+-}
+
+-- Exercicio 8: Implementar uma função que recebe um lista de listas
+-- [[]]  e retorna uma lista.
+
+listToList [] = []
+listToList (x:xs) = x ++ listToList xs
+
+
+-- Exercicio 9: Implementar que recebe 2 listas, e retorne uma lista
+-- com a soma do primeiro elemento da primeira lista, com o primeiro
+-- elemento da segunda lista.
+-- [1,2,3] [4,5,6] >> [5,7,9]
+
+-- Versão com recursão
+
+somaListas [] [] = []
+somaListas [] _ = []
+somaListas _ [] = []
+somaListas (x:xs) (y:ys) = (x+y) : somaListas xs ys
+
+somaListas' xs xy = [x + y | x <- xs, y <- ys]
